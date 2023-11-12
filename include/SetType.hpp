71fea10e9be1c94b2a878248656f6c0ca748b036
sphc <jinkai0916@outlook.com>
2023-11-12 17:17:17 +0800

    add explicit implementation for SetType

--- include/SetType.hpp
+++ include/SetType.hpp
@@ -15,14 +15,6 @@ inline namespace explicit_implementation {
 template <typename ElementType>
 class SetType;
 
-namespace detail {
-template <typename ElementType>
-void __checkOperationSet(const SetType<ElementType> &lhs, const SetType<ElementType> &rhs) {
-    assert(lhs.__elements.size() == rhs.__elements.size());
-    // TODO: 检查输入的两个SetType映射函数一致
-}
-} // namespace detail
-
 template <typename ElementType>
 SetType<ElementType> merge(const SetType<ElementType> &lhs, const SetType<ElementType> &rhs);
 
@@ -31,6 +23,14 @@ SetType<ElementType> intersection(const SetType<ElementType> &lhs, const SetType
 
 template <typename ElementType>
 SetType<ElementType> difference(const SetType<ElementType> &lhs, const SetType<ElementType> &rhs);
+
+namespace detail {
+template <typename ElementType>
+void __checkOperationSet(const dsa::SetType<ElementType> &lhs, const dsa::SetType<ElementType> &rhs) {
+    assert(lhs.__elements.size() == rhs.__elements.size());
+    // TODO: 检查输入的两个SetType映射函数一致
+}
+} // namespace detail
 
 /*
  * @Author       : sphc
@@ -91,7 +91,8 @@ public:
      */
     void remove(const ElementType &e) noexcept;
     /**
-     * @description: 打印集合元素
+     * @description:
+     * @param       {DeMapFunc} 从映射结果反推得到元素的函数
      * @return      {void}
      */
     void print(DeMapFunc deMapFunc) noexcept;
@@ -99,7 +100,7 @@ public:
 private:
     inline static constexpr size_type __default_size{512};
     // 位图，将元素根据映射函数映射到对应的索引处，若元素存在则用 1 标识，否则用 0 标识
-    MapFunc __mapFunc;
+    const MapFunc __mapFunc;
     std::vector<unsigned char> __elements;
 };
 
@@ -156,6 +157,7 @@ void SetType<ElementType>::add(const ElementType &e) noexcept {
     if (__elements.size() == 0) {
         return;
     }
+    // TODO: 映射超出范围，抛出异常
     __elements[__mapFunc(e) % __elements.size()] = 1;
 }
 
@@ -169,11 +171,13 @@ void SetType<ElementType>::remove(const ElementType &e) noexcept {
     if (__elements.size() == 0) {
         return;
     }
+    // TODO: 映射超出范围，抛出异常
     __elements[__mapFunc(e) % __elements.size()] = 0;
 }
 
 /**
- * @description: 打印集合元素
+ * @description:
+ * @param       {DeMapFunc} 从映射结果反推得到元素的函数
  * @return      {void}
  */
 template <typename ElementType>
@@ -190,9 +194,9 @@ template <typename ElementType>
 SetType<ElementType> merge(const SetType<ElementType> &lhs, const SetType<ElementType> &rhs) {
     detail::__checkOperationSet(lhs, rhs);
 
-    SetType<ElementType> ret{lhs.__mapFunc, lhs.__elements.size()};
+    SetType<ElementType> ret{lhs};
     for (typename SetType<ElementType>::size_type i{0}; i < lhs.__elements.size(); ++i) {
-        if (lhs.__elements[i] == 1 || rhs.__elements[i] == 1) {
+        if (rhs.__elements[i] == 1) {
             ret.__elements[i] = 1;
         }
     }
@@ -229,26 +233,191 @@ SetType<ElementType> difference(const SetType<ElementType> &lhs, const SetType<E
 
 namespace implicit_implementation {
 
+template <typename ElementType>
+class SetType;
+
+template <typename ElementType>
+SetType<ElementType> merge(const SetType<ElementType> &lhs, const SetType<ElementType> &rhs);
+
+template <typename ElementType>
+SetType<ElementType> intersection(const SetType<ElementType> &lhs, const SetType<ElementType> &rhs);
+
+template <typename ElementType>
+SetType<ElementType> difference(const SetType<ElementType> &lhs, const SetType<ElementType> &rhs);
+
 /*
  * @Author       : sphc
  * @Date         : 2023-11-10 13:01:53
  * @LastEditors  : sphc
- * @LastEditTime : 2023-11-10 13:14:15
+ * @LastEditTime : 2023-11-10 14:02:07
  * @FilePath     : /include/SetType.hpp
- * @Description  :
+ * @Description  : 隐式实现的集合，可装入任意数量的元素
  */
 template <typename ElementType>
 class SetType {
 public:
+    friend SetType<ElementType> merge<ElementType>(const SetType<ElementType> &lhs, const SetType<ElementType> &rhs);
+    friend SetType<ElementType> intersection<ElementType>(const SetType<ElementType> &lhs, const SetType<ElementType> &rhs);
+    friend SetType<ElementType> difference<ElementType>(const SetType<ElementType> &lhs, const SetType<ElementType> &obj);
+
+    using size_type = std::size_t;
     SetType() = default;
-    SetType(const SetType &) = delete;
-    SetType(SetType &&) = delete;
-    SetType &operator=(const SetType &) = delete;
-    SetType &operator=(SetType &&) = delete;
+    SetType(const SetType &) = default;
+    SetType(SetType &&) = default;
+    SetType &operator=(const SetType &) = default;
+    SetType &operator=(SetType &&) = default;
     ~SetType() = default;
 
+    /**
+     * @description: 将集合置空
+     * @return      {void}
+     */
+    void makeEmpty() noexcept;
+    /**
+     * @description: 判断集合是否为空
+     * @return      {bool} 若集合为空返回 true，否则返回 false
+     */
+    [[nodiscard]] bool isEmpty() const noexcept;
+    /**
+     * @description: 获取集合中元素数量
+     * @return      {size_type} 集合中的元素数量
+     */
+    [[nodiscard]] size_type size() const noexcept;
+    /**
+     * @description: 向集合中添加元素，若集合中已存在该元素，则无任何操作
+     * @param       {ElementType} e 需要添加的元素
+     * @return      {void}
+     */
+    void add(const ElementType &e);
+    /**
+     * @description: 向集合中添加元素，若集合中已存在该元素，则无任何操作
+     * @param       {ElementType} e 需要添加的元素
+     * @return      {void}
+     */
+    void add(ElementType &&e);
+    /**
+     * @description: 从集合中删除元素，若集合中没有该元素，则无任何操作
+     * @param       {ElementType} e 需要删除的元素
+     * @return      {void}
+     */
+    void remove(const ElementType &e);
+    /**
+     * @description:
+     * @return      {void}
+     */
+    void print() noexcept;
+
 private:
+    std::vector<ElementType> __elements;
 };
+
+/**
+ * @description: 将集合置空
+ * @return      {void}
+ */
+template <typename ElementType>
+void SetType<ElementType>::makeEmpty() noexcept {
+    __elements.clear();
+}
+
+/**
+ * @description: 判断集合是否为空
+ * @return      {bool} 若集合为空返回 true，否则返回 false
+ */
+template <typename ElementType>
+[[nodiscard]] bool SetType<ElementType>::isEmpty() const noexcept {
+    return __elements.empty();
+}
+
+/**
+ * @description: 获取集合中元素数量
+ * @return      {size_type} 集合中的元素数量
+ */
+template <typename ElementType>
+[[nodiscard]] typename SetType<ElementType>::size_type SetType<ElementType>::size() const noexcept {
+    return __elements.size();
+}
+
+/**
+ * @description: 向集合中添加元素，若集合中已存在该元素，则无任何操作
+ * @param       {ElementType} e 需要添加的元素
+ * @return      {void}
+ */
+template <typename ElementType>
+void SetType<ElementType>::add(const ElementType &e) {
+    __elements.push_back(e);
+}
+
+/**
+ * @description: 向集合中添加元素，若集合中已存在该元素，则无任何操作
+ * @param       {ElementType} e 需要添加的元素
+ * @return      {void}
+ */
+template <typename ElementType>
+void SetType<ElementType>::add(ElementType &&e) {
+    __elements.push_back(std::move(e));
+}
+
+/**
+ * @description: 从集合中删除元素，若集合中没有该元素，则无任何操作
+ * @param       {ElementType} e 需要删除的元素
+ * @return      {void}
+ */
+template <typename ElementType>
+void SetType<ElementType>::remove(const ElementType &e) {
+    // TODO: 二分查找
+    auto it{std::find(std::cbegin(__elements), std::cend(__elements), e)};
+    if (it != std::cend(__elements)) {
+        __elements.erase(it);
+    }
+}
+
+/**
+ * @description:
+ * @return      {void}
+ */
+template <typename ElementType>
+void SetType<ElementType>::print() noexcept {
+    std::for_each(std::cbegin(__elements), std::cend(__elements), [](const auto &e) { std::cout << e << " "; });
+    std::cout << std::endl;
+}
+
+template <typename ElementType>
+SetType<ElementType> merge(const SetType<ElementType> &lhs, const SetType<ElementType> &rhs) {
+    // TODO: 使用二分查找，在元素多的表里面查
+    SetType<ElementType> ret{lhs};
+    std::for_each(std::cbegin(rhs.__elements), std::cend(rhs.__elements), [&ret](const auto &e) {
+        if (std::find(std::cbegin(ret.__elements), std::cend(ret.__elements), e) == std::cend(ret.__elements)) {
+            ret.__elements.push_back(e);
+        }
+    });
+    return ret;
+}
+
+template <typename ElementType>
+SetType<ElementType> intersection(const SetType<ElementType> &lhs, const SetType<ElementType> &rhs) {
+    // TODO: 使用二分查找，在元素多的表里面查
+    SetType<ElementType> ret{};
+    std::for_each(std::cbegin(rhs.__elements), std::cend(rhs.__elements), [&ret, &lhs](const auto &e) {
+        if (std::find(std::cbegin(lhs.__elements), std::cend(lhs.__elements), e) != std::cend(lhs.__elements)) {
+            ret.__elements.push_back(e);
+        }
+    });
+    return ret;
+}
+
+template <typename ElementType>
+SetType<ElementType> difference(const SetType<ElementType> &lhs, const SetType<ElementType> &rhs) {
+    // TODO: 使用二分查找，在元素多的表里面查
+    SetType<ElementType> ret{lhs};
+    std::for_each(std::cbegin(rhs.__elements), std::cend(rhs.__elements), [&ret](const auto &e) {
+        auto it{std::find(std::cbegin(ret.__elements), std::cend(ret.__elements), e)};
+        if (it != std::cend(ret.__elements)) {
+            ret.__elements.erase(it);
+        }
+    });
+    return ret;
+}
 
 } // namespace implicit_implementation
 
--- test/testSetType.cpp
+++ test/testSetType.cpp
@@ -2,7 +2,139 @@
 #include <cassert>
 #include <iostream>
 
-int main() {
+void testImplicitImplementation() {
+    dsa::implicit_implementation::SetType<char> setA{};
+    assert(setA.isEmpty());
+    assert(setA.size() == 0);
+    setA.add('b');
+    setA.add('c');
+    setA.add('d');
+    setA.add('a');
+    setA.add('e');
+    setA.add('g');
+    setA.add('f');
+    setA.add('h');
+    setA.add('k');
+    setA.add('j');
+    setA.add('i');
+    setA.add('l');
+    assert(setA.size() == 12);
+    std::cout << "setA: " << std::endl;
+    std::cout << "  expect  : a b c d e f g h i j k l" << std::endl;
+    std::cout << "  real    : ";
+    setA.print();
+
+    dsa::implicit_implementation::SetType<char> setB{};
+    assert(setB.isEmpty());
+    assert(setB.size() == 0);
+    setB.add('d');
+    setB.add('a');
+    setB.add('c');
+    setB.add('j');
+    setB.add('k');
+    setB.add('m');
+    setB.add('n');
+    setB.add('o');
+    setB.add('p');
+    setB.add('q');
+    setB.add('r');
+    setB.add('s');
+    setB.add('t');
+    setB.add('u');
+    assert(setB.size() == 14);
+    std::cout << "setB: " << std::endl;
+    std::cout << "  expect  : a c d j k m n o p q r s t u" << std::endl;
+    std::cout << "  real    : ";
+    setB.print();
+
+    dsa::implicit_implementation::SetType<char> mergeAB{dsa::implicit_implementation::merge(setA, setB)};
+    assert(mergeAB.size() == 21);
+    std::cout << "mergeAB: " << std::endl;
+    std::cout << "  expect  : a b c d e f g h i j k l m n o p q r s t u" << std::endl;
+    std::cout << "  real    : ";
+    mergeAB.print();
+    mergeAB.add('v');
+    mergeAB.add('w');
+    mergeAB.add('x');
+    mergeAB.add('y');
+    mergeAB.add('z');
+    std::cout << "mergeAB: " << std::endl;
+    std::cout << "  expect  : a b c d e f g h i j k l m n o p q r s t u v w x y z" << std::endl;
+    std::cout << "  real    : ";
+    mergeAB.print();
+
+    dsa::implicit_implementation::SetType<char> mergeBA{dsa::implicit_implementation::merge(setB, setA)};
+    assert(mergeBA.size() == 21);
+    std::cout << "mergeBA: " << std::endl;
+    std::cout << "  expect  : a b c d e f g h i j k l m n o p q r s t u" << std::endl;
+    std::cout << "  real    : ";
+    mergeBA.print();
+    mergeBA.add('v');
+    mergeBA.add('w');
+    mergeBA.add('x');
+    mergeBA.add('y');
+    mergeBA.add('z');
+    std::cout << "mergeBA: " << std::endl;
+    std::cout << "  expect  : a b c d e f g h i j k l m n o p q r s t u v w x y z" << std::endl;
+    std::cout << "  real    : ";
+    mergeBA.print();
+
+    dsa::implicit_implementation::SetType<char> intersectionAB{dsa::implicit_implementation::intersection(setA, setB)};
+    assert(intersectionAB.size() == 5);
+    std::cout << "intersectionAB: " << std::endl;
+    std::cout << "  expect  : a c d j k" << std::endl;
+    std::cout << "  real    : ";
+    intersectionAB.print();
+    intersectionAB.remove('j');
+    intersectionAB.remove('k');
+    assert(intersectionAB.size() == 3);
+    std::cout << "intersectionAB: " << std::endl;
+    std::cout << "  expect  : a c d" << std::endl;
+    std::cout << "  real    : ";
+    intersectionAB.print();
+    intersectionAB.remove('c');
+    intersectionAB.remove('a');
+    intersectionAB.remove('d');
+    assert(intersectionAB.isEmpty());
+    intersectionAB.remove('c');
+    assert(intersectionAB.isEmpty());
+
+    dsa::implicit_implementation::SetType<char> intersectionBA{dsa::implicit_implementation::intersection(setB, setA)};
+    assert(intersectionBA.size() == 5);
+    std::cout << "intersectionBA: " << std::endl;
+    std::cout << "  expect  : a c d j k" << std::endl;
+    std::cout << "  real    : ";
+    intersectionBA.print();
+    intersectionBA.remove('j');
+    intersectionBA.remove('k');
+    assert(intersectionBA.size() == 3);
+    std::cout << "intersectionBA: " << std::endl;
+    std::cout << "  expect  : a c d" << std::endl;
+    std::cout << "  real    : ";
+    intersectionBA.print();
+    intersectionBA.remove('c');
+    intersectionBA.remove('a');
+    intersectionBA.remove('d');
+    assert(intersectionBA.isEmpty());
+    intersectionBA.remove('c');
+    assert(intersectionBA.isEmpty());
+
+    dsa::implicit_implementation::SetType<char> differenceAB{dsa::implicit_implementation::difference(setA, setB)};
+    assert(differenceAB.size() == 7);
+    std::cout << "differenceAB: " << std::endl;
+    std::cout << "  expect  : b e f g h i l" << std::endl;
+    std::cout << "  real    : ";
+    differenceAB.print();
+
+    dsa::implicit_implementation::SetType<char> differenceBA{dsa::implicit_implementation::difference(setB, setA)};
+    assert(differenceBA.size() == 9);
+    std::cout << "differenceBA: " << std::endl;
+    std::cout << "  expect  : m n o p q r s t u" << std::endl;
+    std::cout << "  real    : ";
+    differenceBA.print();
+}
+
+void testExplicitImplementation() {
     dsa::SetType<char> setA{[](const auto &c) { return c - 'a'; }, 26};
     assert(setA.isEmpty());
     assert(setA.size() == 0);
@@ -138,5 +270,10 @@ int main() {
     std::cout << "  expect  : m n o p q r s t u" << std::endl;
     std::cout << "  real    : ";
     differenceBA.print([](size_t i) { return 'a' + i; });
+}
+
+int main() {
+    testImplicitImplementation();
+    // testExplicitImplementation();
     return 0;
 }
